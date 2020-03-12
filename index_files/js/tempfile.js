047eee0dceaf1c1432b5afe58094d24c303a6b1b
Tarun Mahajan <tarunm201412@gmail.com>
2020-03-12 14:16:36 +0530

    TempFile created

---
+++ index_files/js/tempfile.js
@@ -0,0 +1,28 @@
+Please enter in your domain name(s) (comma and/or space separated)  (Enter 'c'
+to cancel): oxalislinux.hubbroker1.net
+Obtaining a new certificate
+Performing the following challenges:
+http-01 challenge for oxalislinux.hubbroker1.net
+Waiting for verification...
+Cleaning up challenges
+Failed authorization procedure. oxalislinux.hubbroker1.net (http-01): urn:ietf:params:acme:error:unauthorized :: The client lacks sufficient authorization :: Invalid response from https://oxalislinux.hubbroker1.net/.well-known/acme-challenge/4AKe26a8KN8l5sEXvZkV9hfrNdOjOsuB8Yatrq9jojM [52.158.33.205]: "<!DOCTYPE html>\r\n<html lang=\"en\">\r\n<head>\r\n  <meta charset=\"utf-8\">\r\n  <meta http-equiv=\"x-ua-compatible\" content=\"ie=edge\">\r\n  "
+
+IMPORTANT NOTES:
+ - The following errors were reported by the server:
+
+   Domain: oxalislinux.hubbroker1.net
+   Type:   unauthorized
+   Detail: Invalid response from
+   https://oxalislinux.hubbroker1.net/.well-known/acme-challenge/4AKe26a8KN8l5sEXvZkV9hfrNdOjOsuB8Yatrq9jojM
+   [52.158.33.205]: "<!DOCTYPE html>\r\n<html
+   lang=\"en\">\r\n<head>\r\n  <meta charset=\"utf-8\">\r\n  <meta
+   http-equiv=\"x-ua-compatible\" content=\"ie=edge\">\r\n  "
+
+   To fix these errors, please make sure that your domain name was
+   entered correctly and the DNS A/AAAA record(s) for that domain
+   contain(s) the right IP address.
+
+
+<form name="contact-form" class="contact-me" id="gform" method="POST" action="https://script.google.com/macros/s/AKfycbz-jEB8UnL59UBZj6Qd2Mtp_LjDjEzkHUMvqmJ3Bw/exec">
+
+Temporary command.
